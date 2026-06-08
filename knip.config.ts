46b2d05c9c93b8c32a9c23b7e6b195a3a8867c24
artiphishle <fabio.gartenmann@protonmail.com>
2026-06-08 23:48:30 +0200

    chore: fix knip config

--- knip.config.ts
+++ knip.config.ts
@@ -1,12 +1,10 @@
 import { createKnipConfig } from '@ankhorage/devtools/knip';
 
 export default createKnipConfig({
-  entry: ['examples/basic-app/App.tsx'],
+  entry: ['examples/basic/App.tsx'],
   ignoreFiles: [
     '.prettierrc.js',
     'eslint.config.mjs',
-    'examples/expo-showcase/**',
-    'examples/*/*/**',
     'paradox.config.ts',
   ],
 });
